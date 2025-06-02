df52b98e73e1aefef0204e721bc35779406fbfdd
yuntae1116 <wndbsxo1116@yonsei.ac.kr>
2025-06-02 14:26:53 +0900

    Update swimming.py

--- swimming.py
+++ swimming.py
@@ -1,17 +1,14 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, KFold, RandomizedSearchCV
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.neural_network import MLPRegressor
 from scipy.stats import randint, uniform
-import seaborn as sns
 
 df = pd.read_csv("longitudinal_athlete_data_with_RT_corrected.csv")
 
